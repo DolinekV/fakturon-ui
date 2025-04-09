a7f35ffb90d51050d8963799de6b3de4b21ad1fb
Vit Dolinek <dolinekvit@gmail.com>
2025-04-09 23:19:14 +0200

    Remove dist

--- dist/dist/src/models/customer.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CUSTOMER_FORM_FIELDS = void 0;
-exports.CUSTOMER_FORM_FIELDS = ["name", "cin", "tin", "phone", "email"];
-//# sourceMappingURL=customer.js.map
